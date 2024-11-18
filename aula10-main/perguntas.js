dc123094d2264086f24437d2cc0ab6433aee96bd
divileski77 <163847612+divileski7@users.noreply.github.com>
2024-11-18 08:42:43 -0300

    Add files via upload

---
+++ aula10-main/perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Geografia',
+    'Qual o maior país do mundo?',
+    'Russia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia '
+)
+
+criaCartao(
+    'Futebol',
+    'quantas libertadores o Grêmio Foot-Ball Porto Alegrense tem?',
+    'Trẽs libertadores.'
+)
+criaCartao(
+    'Futebol',
+    'Qual o maior clube do sul do Brasil?',
+    'Grêmio Foot-Ball Porto Alegrense'
+)
